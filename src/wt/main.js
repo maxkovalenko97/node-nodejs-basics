2d166dca8e640fced1327cf59f5a9b8c5c81e4cb
Max Kovalenko <tutmrmaxi@gmail.com>
2023-06-19 22:47:50 +0300

    feat: implement advance functions

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,31 @@
+import { Worker } from 'worker_threads';
+import { cpus } from 'os';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const workerFile = join(__dirname, 'worker.js');
+
 const performCalculations = async () => {
-    // Write your code here
+    let incNumber = 25;
+
+    const workerResults = await Promise.allSettled(cpus().map(() => {
+        return new Promise((result, reject) => {
+            const worker = new Worker(workerFile, { workerData: incNumber++ });
+            worker.on('message', message => result(message));
+            worker.on('error', error => reject(error));
+        });
+    }))
+
+    const resultsArr = workerResults.map(({ status, value }) => {
+        if (status === 'fulfilled') {
+            return { status: 'resolved', data: value };
+        } else {
+            return { status: 'error', data: null };
+        }
+    });
+
+    console.log(resultsArr);
 };
 
 await performCalculations();
